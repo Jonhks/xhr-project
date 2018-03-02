284b68e20e863a1f3fd146486f627a9857dfd6c5
scarlett07 <scarlett.geo@gmail.com>
2018-03-02 09:20:55 -0600

    usando un template

--- js/app.js
+++ js/app.js
@@ -22,30 +22,36 @@ function addNews() {
   const data = JSON.parse(this.responseText);
   const articles = data.response.docs;
   console.log(articles);
+  let notice=' ';
   articles.forEach(element => {
-        let title = element.headline.main;
-        let snippet = element.snippet;
-        let img = `https://www.nytimes.com/${element.multimedia[1].legacy.wide}`;
-        let li = document.createElement('li');
-        let photo = document.createElement('img');
-        let span = document.createElement('span');
-        let resumen = document.createElement('p');
-        let href = element.web_url;
-        console.log(href);
-        li.className = 'collection-item avatar';
-        photo.src=img;
-        photo.className = 'circle'
-        span.innerText = title;
-        span.className = 'title';
-        span.href= `${href}`;
-        resumen.innerText = snippet;
-        responseContainer.appendChild(li);
-        li.appendChild(photo);
-        li.appendChild(span);
-        li.appendChild(resumen);
-      })
-    };
+    let title = element.headline.main;
+    let snippet = element.snippet;
+    let img = `https://www.nytimes.com/${element.multimedia[1].legacy.wide}`;
+    notice += `<li class="collection-item avatar">
+                    <img src=${img} alt="" class="circle">
+                    <a href="#!" class="title">${title}<span class="title">Title</span></a>
+                    <p>${snippet}</p>
+                  </li>`;
+                  console.log(notice);
+    // let li = document.createElement('li');
+    // let photo = document.createElement('img');
+    // let span = document.createElement('span');
+    // let resumen = document.createElement('p');
+    // let href = element.web_url;
+    // li.className = 'collection-item avatar';
+    // photo.src = img;
+    // photo.className = 'circle'
+    // span.innerText = title;
+    // span.className = 'title';
+    // photo.setAttribute('href', href)
+    // resumen.innerText = snippet;
+     responseContainer.innerHTML=notice;
+    //li.appendChild(photo);
+    //li.appendChild(span);
+  //  li.appendChild(resumen);
+  })
+};
 
-      function hanleError() {
-        console.log('Se ha presentado un error');
-      };
+function hanleError() {
+  console.log('Se ha presentado un error');
+};
